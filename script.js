5d6f1146947fd20157a694ac9cb5f255279d6092
IflameI <67149445+IflameI@users.noreply.github.com>
2020-12-04 18:23:34 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -119,10 +119,6 @@ function isiPhone(){
     var is_safari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
     var isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || (typeof safari !== 'undefined' && safari.pushNotification));
 
-    if(isSafari){
-        $('.container').css({zoom : '1.2'});
-        $('.wrapper__aside').css({height : '100vh'});
-    }
 
 });
 
